f048029cf7556b0b0b15363b0f95d47e104ef6e6
paige-clark <paigeclarkillustration@gmail.com>
2022-12-21 22:14:07 -0500

    Complete exercise 2

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,21 +1,42 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+// interface method:
+interface NewType {
+  first: number;
+  second: number;
+}
+
+export const addTwoNumbers = (params: NewType) => {
   return params.first + params.second;
 };
+
+// type alias method:
+// type NewType = {
+//   first: number;
+//   second: number;
+// };
+
+// export const addTwoNumbers = (params: NewType) => {
+//   return params.first + params.second;
+// };
+
+// anonymous method:
+// export const addTwoNumbers = (params: { first: number; second: number }) => {
+//   return params.first + params.second;
+// };
 
 it("Should add the two numbers together", () => {
   expect(
     addTwoNumbers({
       first: 2,
       second: 4,
-    }),
+    })
   ).toEqual(6);
 
   expect(
     addTwoNumbers({
       first: 10,
       second: 20,
-    }),
+    })
   ).toEqual(30);
 });
